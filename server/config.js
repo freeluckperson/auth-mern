ecd0d2a6066bce19c51795103dd7d2eb549c448f
erickubuntu <gestionecesaria@gmail.com>
2024-02-12 20:48:12 -0400

    resolviendo problemas de cors part2

--- server/config.js
+++ server/config.js
@@ -1,4 +1,2 @@
-export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret123";
-export const DB_URL =
-  process.env.DB_URL ||
-  "mongodb+srv://pomodoro:1one2two@auth.nku4meh.mongodb.net/";
+export const TOKEN_SECRET = process.env.TOKEN_SECRET;
+export const DB_URL = process.env.DB_URL;
