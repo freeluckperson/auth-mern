1c12b0e0011a7650fd4de0845665fd45fb213852
erickubuntu <gestionecesaria@gmail.com>
2024-02-12 20:02:58 -0400

    modificando cors para que acepte cualquier peticion

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: "https://server-express-docker.onrender.com/api/",
   withCredentials: true,
 });
 
